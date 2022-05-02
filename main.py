61806185f163a9160ac67b4b7918c7aca8dfdfa5
Lorelorelore <malore.delacuesta@gmail.com>
2022-05-02 13:55:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -139,18 +139,21 @@ def app_object_detection():
             
             classes2, scores2, boxes2 = model2.detect(
                 image, Conf_threshold, NMS_threshold)
-        
+            
+            # centerCoord = (boxes[0]+(boxes[2]/2), boxes[1]+(boxes[3]/2))
+            
             for (classid, score, box) in zip(classes, scores, boxes):
                 if classid == 0:
                     print(box)
-                    # centerCoord = (box[0]+(box[2]/2), box[1]+(box[3]/2))
-                    # print (centerCoord)
+                    #box = [ x=12  y=101  w=379 h=259]
+
                     color = COLORS[int(classid) % len(COLORS)]
+
                     label = "%s : %f" % (class_name[classid[0]], score)
+
                     cv2.rectangle(image, box, color, 1)
                     cv2.putText(image, label, (box[0], box[1]-10),
                                 cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
-                    # cv2.circle(image, , 5, color, 1)
                     
 
             for (classid, score, box) in zip(classes2, scores2, boxes2):
